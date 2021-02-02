d8ebda0ead21e12f47b33f1cf6645139f1b8531f
Berna Yılmaz <berna14y@gmail.com>
2021-02-02 18:34:37 +0300

    Add project files.

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Id + customer.FirstName + customer.LastName + "Added to the system.\n");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Id + customer.FirstName + customer.LastName + "Deleted from the system.\n");
+
+        }
+        public void List(List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            
+            {
+                Console.WriteLine("Id :" + customer.Id);
+                Console.WriteLine("First Name :" +customer.FirstName);
+                Console.WriteLine("Last Name :" + customer.LastName);
+                Console.WriteLine("PhoneNumber :" + customer.PhoneNumber);
+                Console.WriteLine("Email :" + customer.Email);
+                Console.WriteLine("Adress :" + customer.Adress);
+            }
+        }
+          
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            {
+                Id = 123456,
+                FirstName = "Berna",
+                LastName = "Yılmaz",
+                PhoneNumber = "+90 534 123 45 11",
+                Email = "123@gmail.com",
+                Adress = "Antalya"
+
+            };
+
+            Customer customer2 = new Customer()
+            {
+                Id = 536482,
+                FirstName = "Nergiz",
+                LastName = "Yıldırım",
+                PhoneNumber = "+90 531 111 11 00",
+                Email = "bnbn@gmail.com",
+                Adress = "Eskişehir"
+
+            };
+
+            Customer customer3 = new Customer()
+            {
+                Id = 789635,
+                FirstName = "Ahmet",
+                LastName = "Kılıç",
+                PhoneNumber = "+90 541 256 33 14",
+                Email = "hgn1@gmail.com",
+                Adress = "Van"
+            };
+
+            List<Customer> customers = new List<Customer> { customer1, customer2, customer3 };
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.Delete(customer1);
+            customerManager.Delete(customer2);
+            customerManager.Delete(customer3);
+            customerManager.List(customers);
+        }
+    }
+}
